e70bf3fb8f9d45e020ee183f1f7e521309303ed6
mafarhan72 <mafarhan1234@gmail.com>
2026-04-28 10:41:33 -0400

    deleting

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,29 +1,29 @@
-data "archive_file" "s3_lambda_code"{
-  type        = "zip"
-  source_file = "${path.module}/lambda/s3.py"
-  output_path = "${path.module}/lambda/function.zip"
-}
+# data "archive_file" "s3_lambda_code"{
+#   type        = "zip"
+#   source_file = "${path.module}/lambda/s3.py"
+#   output_path = "${path.module}/lambda/function.zip"
+# }
 
 
 
-resource "aws_lambda_function" "example" {
-  filename      = data.archive_file.s3_lambda_code.output_path
-  function_name = "example_lambda_function"
-  role          = "arn:aws:iam::541804384520:role/lambda"
-  handler       = "s3.lambda_handler"
-  code_sha256   = data.archive_file.s3_lambda_code.output_base64sha256
+# resource "aws_lambda_function" "example" {
+#   filename      = data.archive_file.s3_lambda_code.output_path
+#   function_name = "example_lambda_function"
+#   role          = "arn:aws:iam::541804384520:role/lambda"
+#   handler       = "s3.lambda_handler"
+#   code_sha256   = data.archive_file.s3_lambda_code.output_base64sha256
 
-  runtime = "python3.12"
+#   runtime = "python3.12"
 
-  environment {
-    variables = {
-      ENVIRONMENT = "production"
-      LOG_LEVEL   = "info"
-    }
-  }
+#   environment {
+#     variables = {
+#       ENVIRONMENT = "production"
+#       LOG_LEVEL   = "info"
+#     }
+#   }
 
-  tags = {
-    Environment = "production"
-    Application = "example"
-  }
-}
+#   tags = {
+#     Environment = "production"
+#     Application = "example"
+#   }
+# }
--- terraform/sns.tf
+++ terraform/sns.tf
@@ -1,16 +1,16 @@
-resource "aws_sns_topic" "user_updates" {
-  name = "user-updates-topic"
-}
+# resource "aws_sns_topic" "user_updates" {
+#   name = "user-updates-topic"
+# }
 
 
 
-resource "aws_sns_topic_subscription" "email_subscription" {
-    for_each = toset([
-        "mohammedabdulfarhan17@gmail.com",
-        "mafarhan1234@gmail.com"
-    ])
-  topic_arn            = aws_sns_topic.user_updates.arn
-  protocol             = "email"
-  endpoint             = each.value
+# resource "aws_sns_topic_subscription" "email_subscription" {
+#     for_each = toset([
+#         "mohammedabdulfarhan17@gmail.com",
+#         "mafarhan1234@gmail.com"
+#     ])
+#   topic_arn            = aws_sns_topic.user_updates.arn
+#   protocol             = "email"
+#   endpoint             = each.value
   
-}
+# }
